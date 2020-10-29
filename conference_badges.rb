b9e0cb9c06613fd8474baab9f3e2ae3e32668961
Tres Tarwater <trest03@gmail.com>
2020-10-29 19:04:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,9 @@ def batch_badge_creator(attendees)
   end
   badge_messages
 end
+
+badge_messages = ["Red", "Yellow", "Blue"]
+primary_colors.each do |color| # do begins a block
+  # the lines between the do/end are the block's body
+  puts "Primary Color #{color} is #{color.length} letters long."
+end 
