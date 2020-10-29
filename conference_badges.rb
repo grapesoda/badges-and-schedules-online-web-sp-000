5712bd6fca6dbc672894506e7e22c52ea590e4fa
Tres Tarwater <trest03@gmail.com>
2020-10-29 18:54:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,13 @@ def batch_badge_creator(attendees)
   end
   badge_messages
 end
+
+def batch_badge_creator(attendees)
+  msg = []
+  attendees.each do |name|
+    name.each do |n|
+      msg << "Hello, my name is #{n}"
+    end
+  end
+  msg
+end
